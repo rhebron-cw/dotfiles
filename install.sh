f0527822736c6e26f1a1af85e346d742ec8abcb6
rhebron-cw <rhebron@coreweave.com>
2026-05-04 03:36:37 +0000

    reordered install.sh

--- install.sh
+++ install.sh
@@ -2,4 +2,6 @@
 git clone git@github.com:rhebron-cw/jira_autobot.git
 cd jira_autobot
 docker compose up --build -d
+sleep 30
+curl -s --form-string "token=$po_app" --form-string "user=$po_usr" --form-string "message=install.sh completed. \n $(docker ps)" https://api.pushover.net/1/messages.json
 echo "$(date) install.sh ran successfully" >> ~/post_install.log
