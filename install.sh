15780980c7bcd8337c1e14445c6bf8beaaee2f00
rhebron-cw <rhebron@coreweave.com>
2026-05-04 03:03:58 +0000

    fixed install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,5 @@
-#apt install cowsay -y
+#!/usr/bin/zsh
+git clone git@github.com:rhebron-cw/jira_autobot.git
+cd jira_autobot
+docker compose up --build -d
+echo "$(date) install.sh ran successfully" >> ~/post_install.log
