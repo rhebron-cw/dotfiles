8374a7a094797104127734159b58fb124bccef84
rhebron-cw <rhebron@coreweave.com>
2026-05-19 16:19:14 -0500

    Update install.sh
    testting again

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/zsh
 #sudo timedatectl set-timezone America/Chicago
-git clone git@github.com:rhebron-cw/jira_autobot.git
-cd jira_autobot
+#git clone git@github.com:rhebron-cw/jira_autobot.git
+#cd jira_autobot
 #docker compose up --build -d
 #sleep 30
 # curl -s --form-string "token=$po_app" --form-string "user=$po_usr" --form-string "message=install.sh completed. \n $(docker ps)" https://api.pushover.net/1/messages.json >> ~/post_install.log 2>&1
