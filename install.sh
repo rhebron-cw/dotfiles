61739bb636fc6caaa3e1147eb03c08ca3b57aa37
rhebron-cw <rhebron@coreweave.com>
2026-05-21 19:12:14 +0000

    uncommented install.sh so docker will be run automatically again.

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/zsh
 #sudo timedatectl set-timezone America/Chicago
-#git clone git@github.com:rhebron-cw/jira_autobot.git
-#cd jira_autobot
-#docker compose up --build -d
-#sleep 30
-# curl -s --form-string "token=$po_app" --form-string "user=$po_usr" --form-string "message=install.sh completed. \n $(docker ps)" https://api.pushover.net/1/messages.json >> ~/post_install.log 2>&1
+git clone git@github.com:rhebron-cw/jira_autobot.git
+cd jira_autobot
+docker compose up --build -d
+sleep 30
+ curl -s --form-string "token=$po_app" --form-string "user=$po_usr" --form-string "message=install.sh completed. \n $(docker ps)" https://api.pushover.net/1/messages.json >> ~/post_install.log 2>&1
 echo "$(date) install.sh ran successfully" >> ~/post_install.log 2>&1
