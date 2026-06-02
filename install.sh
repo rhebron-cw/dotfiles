30b5126b390d0ff0e58217f1698d68a266111b91
rhebron-cw <rhebron@coreweave.com>
2026-06-02 13:38:47 +0000

    Added logic to detect if the git repo already exists. if it is it'll update it

--- install.sh
+++ install.sh
@@ -1,8 +1,17 @@
-#!/usr/bin/zsh
-set -e
+#!/usr/bin/env zsh
+set -euo pipefail
 
-git clone git@github.com:rhebron-cw/jira_autobot.git >> ~/post_install.log 2>&1
-cd jira_autobot
-docker-compose up --build -d >> ~/post_install.log 2>&1
-echo "$(date) install.sh ran successfully" >> ~/post_install.log 2>&1
+LOG=~/post_install.log
+REPO_DIR=~/jira_autobot
+
+if [ -d "$REPO_DIR/.git" ]; then
+  echo "$(date) jira_autobot already exists, updating" >> "$LOG"
+  git -C "$REPO_DIR" pull >> "$LOG" 2>&1
+else
+  git clone git@github.com:rhebron-cw/jira_autobot.git "$REPO_DIR" >> "$LOG" 2>&1
+fi
+
+docker-compose -f "$REPO_DIR/compose.yml" up --build -d >> "$LOG" 2>&1
+echo "$(date) install.sh ran successfully" >> "$LOG"
+
 # curl -s --form-string "token=$po_app" --form-string "user=$po_usr" --form-string "message=install.sh completed. \n $(docker ps)" https://api.pushover.net/1/messages.json >> ~/post_install.log 2>&1
