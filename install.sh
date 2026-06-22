fbb4c900e1cc2ad2cebdf2ddf36f581384450663
rhebron-cw <rhebron@coreweave.com>
2026-06-22 13:26:56 -0500

    Update install.sh to install dotfiles.

--- install.sh
+++ install.sh
@@ -2,18 +2,33 @@
 set -euo pipefail
 
 LOG=~/post_install.log
-REPO_DIR=~/jira_autobot
+REPO_DIR1=~/dotfiles
+REPO_DIR2=~/jira_autobot
 
-if [ -d "$REPO_DIR/.git" ]; then
-  echo "$(date) jira_autobot already exists, updating" >> "$LOG"
-  git -C "$REPO_DIR" pull >> "$LOG" 2>&1
+if [ -d "~/.ssh/known_hosts" ]; then
+  echo "$(date) ssh/known_hosts exists, continuing" >> "$LOG"
 else
+  echo "$(date) created ssh/known_hosts and added github fingerprint" >> "$LOG"
   mkdir ~/.ssh
   ssh-keyscan github.com >> ~/.ssh/known_hosts
+fi
+
+if [ -d "$REPO_DIR1/.git" ]; then
+  echo "$(date) dotfiles already exists, updating" >> "$LOG"
+  git -C "$REPO_DIR1" pull >> "$LOG" 2>&1
+else
+  git clone git@github.com:rhebron-cw/dotfiles.git "$REPO_DIR" >> "$LOG" 2>&1
+fi
+
+if [ -d "$REPO_DIR2/.git" ]; then
+  echo "$(date) jira_autobot already exists, updating" >> "$LOG"
+  git -C "$REPO_DIR2" pull >> "$LOG" 2>&1
+else
   git clone git@github.com:rhebron-cw/jira_autobot.git "$REPO_DIR" >> "$LOG" 2>&1
 fi
 
-docker-compose -f "$REPO_DIR/compose.yml" up --build -d >> "$LOG" 2>&1
+cp ~/dotfiles/.* ~/
+docker-compose -f "$REPO_DIR2/compose.yml" up --build -d >> "$LOG" 2>&1
 echo "$(date) install.sh ran successfully" >> "$LOG"
 
 # curl -s --form-string "token=$po_app" --form-string "user=$po_usr" --form-string "message=install.sh completed. \n $(docker ps)" https://api.pushover.net/1/messages.json >> ~/post_install.log 2>&1
