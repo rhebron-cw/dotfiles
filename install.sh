744f0a5c110ac804ccc90d7460e82a2b908cd9c2
rhebron-cw <rhebron@coreweave.com>
2026-06-22 12:28:37 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,8 @@ if [ -d "$REPO_DIR/.git" ]; then
   echo "$(date) jira_autobot already exists, updating" >> "$LOG"
   git -C "$REPO_DIR" pull >> "$LOG" 2>&1
 else
-  ssh-keyscan git@github.com> >> ~/.ssh/known_hosts
+  mkdir ~/.ssh
+  ssh-keyscan git@github.com >> ~/.ssh/known_hosts
   git clone git@github.com:rhebron-cw/jira_autobot.git "$REPO_DIR" >> "$LOG" 2>&1
 fi
 
