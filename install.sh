57ebd55fc2d86274a9f9e99aa1dfed542549cd5e
Ryan Hebron <rhebron@coreweave.com>
2026-07-01 22:43:49 +0000

    updated incomplete logging

--- install.sh
+++ install.sh
@@ -6,23 +6,23 @@ REPO_DIR1=~/dotfiles
 REPO_DIR2=~/jira_autobot
 
 if [ -d "~/.ssh" ]; then
-  echo "$(date) ssh/known_hosts exists, continuing" >> "$LOG"
-  ssh-keyscan github.com >> ~/.ssh/known_hosts
+  echo "$(date) ssh/known_hosts exists, continuing" >> "$LOG" 2>&1
+  ssh-keyscan github.com >> ~/.ssh/known_hosts >> "$LOG" 2>&1
 else
-  echo "$(date) created ssh/known_hosts and added github fingerprint" >> "$LOG"
-  mkdir ~/.ssh
-  ssh-keyscan github.com >> ~/.ssh/known_hosts
+  echo "$(date) created ssh/known_hosts and added github fingerprint" >> "$LOG" 2>&1
+  mkdir ~/.ssh >> "$LOG" 2>&1
+  ssh-keyscan github.com >> ~/.ssh/known_hosts >> "$LOG" 2>&1
 fi
 
 if [ -d "$REPO_DIR1/.git" ]; then
-  echo "$(date) dotfiles already exists, updating" >> "$LOG"
+  echo "$(date) dotfiles already exists, updating" >> "$LOG" 2>&1
   git -C "$REPO_DIR1" pull >> "$LOG" 2>&1
 else
   git clone git@github.com:rhebron-cw/dotfiles.git "$REPO_DIR1" >> "$LOG" 2>&1
 fi
 
 if [ -d "$REPO_DIR2/.git" ]; then
-  echo "$(date) jira_autobot already exists, updating" >> "$LOG"
+  echo "$(date) jira_autobot already exists, updating" >> "$LOG" 2>&1
   git -C "$REPO_DIR2" pull >> "$LOG" 2>&1
 else
   git clone git@github.com:rhebron-cw/jira_autobot.git "$REPO_DIR2" >> "$LOG" 2>&1
@@ -30,4 +30,4 @@ fi
 
 #cp ~/dotfiles/.*(.) ~/./
 docker-compose -f "$REPO_DIR2/compose.yml" up --build -d >> "$LOG" 2>&1
-echo "$(date) install.sh ran successfully" >> "$LOG"
+echo "$(date) install.sh ran successfully" >> "$LOG" 2>&1
